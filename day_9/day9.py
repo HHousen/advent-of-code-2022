f11d18de3d42288587d1af8b3834fb94cef92277
Hayden Housen <hayden@haydenhousen.com>
2022-12-09 19:33:19 -0500

    Solve part 1 using part 2 algorithm

--- day_9/day9.py
+++ day_9/day9.py
@@ -2,33 +2,7 @@ with open("puzzle_input.txt", "r") as puzzle_input:
     puzzle_input = puzzle_input.read().rstrip()
 
 moves = [x.split(" ") for x in puzzle_input.split("\n")]
-
 directions = {"U": (0, 1), "D": (0, -1), "R": (1, 0), "L": (-1, 0)}
-visited = set()
-
-head_position = (0, 0)
-tail_position = (0, 0)
-previous_direction = None
-for direction, distance in moves:
-    distance = int(distance)
-
-    for _ in range(distance):
-        previous_head_position = head_position
-        head_position = tuple(
-            head_position[i] + directions[direction][i] for i in range(2)
-        )
-        head_tail_distance = [
-            abs(head_position[i] - tail_position[i]) for i in range(2)
-        ]
-        if any(x > 1 for x in head_tail_distance):
-            tail_position = previous_head_position
-        visited.add(tail_position)
-
-
-part1_solution = len(visited)
-
-# Part 1 Solution: 6026
-print(f"Part 1 Solution: {part1_solution}")
 
 
 def sign(num):
@@ -41,7 +15,7 @@ def sign(num):
 
 rope = [[0, 0] for _ in range(10)]
 
-visited = set()
+visited = [set(), set()]
 previous_direction = None
 for direction, distance in moves:
     distance = int(distance)
@@ -75,10 +49,43 @@ for direction, distance in moves:
                 # Update both coordinates in the direction moved.
                 rope[rope_idx][0] += sign(dx)
                 rope[rope_idx][1] += sign(dy)
-        # Keep track of the positions that the last knot visits
-        visited.add(tuple(rope[-1]))
+        # Keep track of the positions that the first knot (excluding the head
+        # of the rope) visits.
+        visited[0].add(tuple(rope[1]))
+        # Keep track of the positions that the last knot visits.
+        visited[1].add(tuple(rope[-1]))
 
-part2_solution = len(visited)
+part1_solution = len(visited[0])
+
+# Part 1 Solution: 6026
+print(f"Part 1 Solution: {part1_solution}")
+
+part2_solution = len(visited[1])
 
 # Part 2 Solution: 2273
 print(f"Part 2 Solution: {part2_solution}")
+
+# This was my original solution to part 1. However, my modified solution for
+# part 2 is capable of solving part 1. This original solution is left here
+# in case it is useful later in a way that part 2's solution is not.
+# visited = set()
+# head_position = (0, 0)
+# tail_position = (0, 0)
+# previous_direction = None
+# for direction, distance in moves:
+#     distance = int(distance)
+
+#     for _ in range(distance):
+#         previous_head_position = head_position
+#         head_position = tuple(
+#             head_position[i] + directions[direction][i] for i in range(2)
+#         )
+#         head_tail_distance = [
+#             abs(head_position[i] - tail_position[i]) for i in range(2)
+#         ]
+#         if any(x > 1 for x in head_tail_distance):
+#             tail_position = previous_head_position
+#         visited.add(tail_position)
+
+
+# part1_solution = len(visited)
